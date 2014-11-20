91aea5befd8d8fc73dccb961301fb3c5b600ec66
jaime <jaimeu@uw.edu>
2014-11-20 08:53:23 -0800

    maps

--- js/app.js
+++ js/app.js
@@ -6,3 +6,14 @@
 // lat: 47.655
 // lng: -122.3080
 
+var mapOptions = {
+	center:{lat: 47.655, lng: -122.3080},
+	zoom: 14 //0=earth to 21 = max zoom
+
+}
+
+//add our map to the page in the 'map' div
+var mapElem = document.getElementById('map');
+
+//create the map
+var map = new google.maps.Map(mapElem, mapOptions);
