5bd4d74edb565cfeeddec58f7a469326899d8791
jaime <jaimeu@uw.edu>
2014-11-20 09:45:56 -0800

    ajax

--- js/app.js
+++ js/app.js
@@ -37,14 +37,38 @@ var marker = new google.maps.Marker({
 //can use this for filters and things
 
 //remove marker from map
-//marker.setMap(null);
+marker.setMap(null);
 
 //recreate the marker
 //marker.setMap(map);
 
 
+
+//create new info winder
+	var infoWin = new google.maps.InfoWindow();
+
 function onGeoPos(position) {
-	console.log(position);
+	console.log("Lat: " + position.coords.latitude);
+	console.log("Lng: " + position.coords.longitude);
+
+	var myLocPos = {
+		lat: position.coords.latitude,
+		lng: position.coords.longitude
+	}
+	var myLocation = new google.maps.Marker({
+		position: myLocPos,
+		map: map
+	});
+
+	
+
+	//set the content (may contain html tags)
+	infoWin.setContent('<p> Hello World! My lat is ' + position.coords.latitude +  'and my lng is '  + position.coords.longitude + '</p>');
+
+	//infoWin.open(map, myLocation);
+
+	//listen for click event on marker
+	google.maps.event.addListener(myLocation, 'click', onMarkerClick)
 
 }
 
@@ -53,9 +77,29 @@ function onGeoErr(error) {
 }
 
 if(navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(onGeoPos, onGeoErr, {enableHighAccuracy: true});
+	navigator.geolocation.getCurrentPosition(onGeoPos, onGeoErr, {enableHighAccuracy: true, maximumAge: 30000});
 
 
 } else {
 	console.log("geolocation not supported");
 }
+
+function onMarkerClick() {
+	//'this' will refer to the marker object
+
+	//pan the map so that the marker is at the center
+	map.panTo(this.getPosition());
+	infoWin.open(map, this);
+}
+
+$.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+	.done(function(data){
+		//success
+		console.log(data);
+	})
+	.fail(function(error) {
+		//error contains error info
+	})
+	.always(function(){
+		//called  either success or error cases
+	})
